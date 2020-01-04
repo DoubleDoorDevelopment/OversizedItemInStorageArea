f3910a6e2e000f20d078145ab3212a3326caee94
Claycorp <Claycorp@doubledoordev.net>
2020-01-03 18:35:05 -0600

    Version bump.

--- src/main/java/net/doubledoordev/OversizedItemInStorageArea.java
+++ src/main/java/net/doubledoordev/OversizedItemInStorageArea.java
@@ -40,7 +40,7 @@ public class OversizedItemInStorageArea
 
     public static final String MOD_ID = "oversizediteminstoragearea";
     public static final String MOD_NAME = "OversizedItemInStorageArea";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.1.1";
     /**
      * This is the instance of your mod as created by Forge. It will never be null.
      */
